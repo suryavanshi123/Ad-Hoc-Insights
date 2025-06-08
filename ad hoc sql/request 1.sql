a780289b72d8fffb7a89443926ecbfdde458ed95
suryavanshi123 <128803320+suryavanshi123@users.noreply.github.com>
2025-06-08 23:42:22 +0530

    Update request 1.sql

--- ad hoc sql/request 1.sql
+++ ad hoc sql/request 1.sql
@@ -1,1 +1,6 @@
-
+/*1. Provide the list of markets in which customer "Atliq Exclusive" operates its
+business in the APAC region.*/
+SELECT market FROM dim_customer 
+WHERE customer = 'Atliq Exclusive' AND region = 'APAC'
+GROUP BY market
+ORDER BY market ;
