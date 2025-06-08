4f08f4f33ab65cfa9b8e776cd98d8d922f755a65
suryavanshi123 <128803320+suryavanshi123@users.noreply.github.com>
2025-06-08 23:49:19 +0530

    Update Request 6.sql

--- ad hoc sql/Request 6.sql
+++ ad hoc sql/Request 6.sql
@@ -5,16 +5,19 @@ customer_code
 customer
 average_discount_percentage*/
 
-WITH TBL1 AS
-(SELECT customer_code AS A, AVG(pre_invoice_discount_pct) AS B FROM fact_pre_invoice_deductions
-WHERE fiscal_year = '2021'
-GROUP BY customer_code),
-     TBL2 AS
-(SELECT customer_code AS C, customer AS D FROM dim_customer
-WHERE market = 'India')
-
-SELECT TBL2.C AS customer_code, TBL2.D AS customer, ROUND (TBL1.B, 4) AS average_discount_percentage
-FROM TBL1 JOIN TBL2
-ON TBL1.A = TBL2.C
-ORDER BY average_discount_percentage DESC
-LIMIT 5 
+SELECT 
+    c.customer_code,
+    c.customer,
+    ROUND(AVG(d.pre_invoice_discount_pct), 4) AS average_discount_percentage
+FROM 
+    fact_pre_invoice_deductions d
+JOIN 
+    dim_customer c ON d.customer_code = c.customer_code
+WHERE 
+    d.fiscal_year = '2021'
+    AND c.market = 'India'
+GROUP BY 
+    c.customer_code, c.customer
+ORDER BY 
+    average_discount_percentage DESC
+LIMIT 5;
